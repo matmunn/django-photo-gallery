654f7625da351c25760cd559d937eea5bdb09f9a
Mat <mat@matmunn.me>
2019-01-10 20:54:42 +1100

    Set slides to not be cover

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1)d9(e1vm^l+=a@h6wx1+obnf&8kild--18+(&0v+uib10ouw0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
